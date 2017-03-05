dc8edae5cca855be879f235c3c3aae64be4c90b5
Edina Morris <edina@Edinas-MacBook-Air.local>
2017-03-05 10:43:45 +0000

    addition of simple cube texturing

--- shaders/PhongVertex.glsl
+++ shaders/PhongVertex.glsl
@@ -1,71 +1,20 @@
-#version 410 core
-/// @brief the vertex passed in
-layout (location = 0) in vec3 inVert;
-/// @brief the normal passed in
-layout (location = 2) in vec3 inNormal;
-/// @brief the in uv
-layout (location = 1) in vec2 inUV;
-/// @brief flag to indicate if model has unit normals if not normalize
-uniform bool Normalize;
-// the eye position of the camera
-uniform vec3 viewerPos;
-/// @brief the current fragment normal for the vert being processed
-out  vec3 fragmentNormal;
+#version 330 core
 
-
-struct Lights
-{
-  vec4 position;
-  vec4 ambient;
-  vec4 diffuse;
-  vec4 specular;
-  float constantAttenuation;
-  float spotCosCutoff;
-  float quadraticAttenuation;
-  float linearAttenuation;
-};
-
-// array of lights
-uniform Lights light;
-// direction of the lights used for shading
-out vec3 lightDir;
-// out the blinn half vector
-out vec3 halfVector;
-out vec3 eyeDirection;
-out vec3 vPosition;
-
-uniform mat4 MV;
+/// @brief MVP passed from app
 uniform mat4 MVP;
-uniform mat3 normalMatrix;
-uniform mat4 M;
-
+// first attribute the vertex values from our VAO
+layout (location=0) in vec3 inVert;
+// second attribute the UV values from our VAO
+layout (location=1)in vec2 inUV;
+// we use this to pass the UV values to the frag shader
+out vec2 vertUV;
 
 void main()
 {
-// calculate the fragments surface normal
-fragmentNormal = (normalMatrix*inNormal);
+// pre-calculate for speed we will use this a lot
 
-
-if (Normalize == true)
-{
- fragmentNormal = normalize(fragmentNormal);
-}
 // calculate the vertex position
-gl_Position = MVP*vec4(inVert,1.0);
-
-vec4 worldPosition = M * vec4(inVert, 1.0);
-eyeDirection = normalize(viewerPos - worldPosition.xyz);
-// Get vertex position in eye coordinates
-// Transform the vertex to eye co-ordinates for frag shader
-/// @brief the vertex in eye co-ordinates  homogeneous
-vec4 eyeCord=MV*vec4(inVert,1);
-
-vPosition = eyeCord.xyz / eyeCord.w;;
-
-float dist;
-
-lightDir=vec3(light.position.xyz-eyeCord.xyz);
-dist = length(lightDir);
-lightDir/= dist;
-halfVector = normalize(eyeDirection + lightDir);
+gl_Position = MVP*vec4(inVert, 1.0);
+// pass the UV values to the frag shader
+vertUV=inUV.st;
 }
--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -7,6 +7,7 @@
 #include <ngl/Material.h>
 #include <ngl/ShaderLib.h>
 #include <QColorDialog>
+#include <QFont>
 
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -20,6 +21,102 @@ NGLScene::NGLScene( QWidget *_parent ) : QOpenGLWidget( _parent )
     m_rain=false;
     m_snow=false;
     m_weatherStrength=0;
+    m_fpsTimer =startTimer(0);
+    m_fps=0;
+    m_frames=0;
+    m_timer.start();
+    m_polyMode=GL_FILL;
+}
+
+void NGLScene::loadTexture()
+{
+    QImage image;
+    bool loaded=image.load("textures/snowflake.bmp");
+    if(loaded == true)
+    {
+        int width=image.width();
+        int height=image.height();
+
+        unsigned char *data = new unsigned char[ width*height*3];
+        unsigned int index=0;
+        QRgb colour;
+        for( int y=0; y<height; ++y)
+        {
+            for( int x=0; x<width; ++x)
+            {
+                colour=image.pixel(x,y);
+
+                data[index++]=qRed(colour);
+                data[index++]=qGreen(colour);
+                data[index++]=qBlue(colour);
+            }
+        }
+
+    glGenTextures(1,&m_textureName);
+    glBindTexture(GL_TEXTURE_2D,m_textureName);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+
+    glGenerateMipmap(GL_TEXTURE_2D); //  Allocate the mipmaps
+
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+/// @brief create a cube and stuff it into a VBO on the GPU
+/// @param[in] _scale a scale factor for the unit vertices
+void NGLScene::createCube( GLfloat _scale )
+{
+
+   // vertex coords array
+    GLfloat vertices[] = {
+                         -1,1,-1,1,1,-1,1,-1,-1, -1,1,-1,-1,-1,-1,1,-1,-1, //back
+                         -1,1,1,1,1,1,1,-1,1, -1,-1,1, 1,-1,1,-1,1,1,  //front
+                         -1,1,-1, 1,1,-1, 1,1,1, -1,1,1, 1,1,1, -1,1,-1, // top
+                         -1,-1,-1, 1,-1,-1, 1,-1,1, -1,-1,1, 1,-1,1, -1,-1,-1, // bottom
+                         -1,1,-1,-1,1,1,-1,-1,-1, -1,-1,-1,-1,-1,1,-1,1,1, // left
+                         1,1,-1,1,1,1,1,-1,-1, 1,-1,-1,1,-1,1,1,1,1, // left
+
+                         };
+    GLfloat texture[] = {
+                        0,0,0,1,1,1 ,0,0,1,0,1,1, //back
+                        0,1,1,0,1,1 ,0,0,1,0,0,1, // front
+                        0,0,1,0,1,1 ,0,1,1,1,0,0, //top
+                        0,0,1,0,1,1 ,0,1,1,1,0,0, //bottom
+                        1,0,1,1,0,0 ,0,0,0,1,1,1, // left
+                        1,0,1,1,0,0 ,0,0,0,1,1,1, // right
+
+                        };
+
+    // first we scale our vertices to _scale
+    for(uint i=0; i<sizeof(vertices)/sizeof(GLfloat); ++i)
+    {
+        vertices[i]*=_scale;
+    }
+
+    // first we create a vertex array Object
+    glGenVertexArrays(1, &m_vaoID);
+
+    // now bind this to be the currently active one
+    glBindVertexArray(m_vaoID);
+    // now we create two VBO's one for each of the objects these are only used here
+    // as they will be associated with the vertex array object
+    GLuint vboID[2];
+    glGenBuffers(2, &vboID[0]);
+    // now we will bind an array buffer to the first one and load the data for the verts
+    glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)*sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    // now we bind the vertex attribute pointer for this object in this case the
+    // vertex data
+    glVertexAttribPointer(0,3,GL_FLOAT,false,0,0);
+    glEnableVertexAttribArray(0);
+    // now we repeat for the UV data using the second VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vboID[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(texture)*sizeof(GLfloat), texture, GL_STATIC_DRAW);
+    glVertexAttribPointer(1,2,GL_FLOAT,false,0,0);
+    glEnableVertexAttribArray(1);
 }
 
 // This virtual function is called once before the first call to paintGL() or resizeGL(),
@@ -47,6 +144,7 @@ void NGLScene::initializeGL()
     constexpr auto shaderProgram = "Phong";
     constexpr auto vertexShader  = "PhongVertex";
     constexpr auto fragShader    = "PhongFragment";
+
     // create the shader program
     shader->createShaderProgram( shaderProgram );
     // now we are going to create empty shaders for Frag and Vert
@@ -67,14 +165,21 @@ void NGLScene::initializeGL()
     shader->linkProgramObject( shaderProgram );
     // and make it active ready to load values
     ( *shader )[ shaderProgram ]->use();
+
     // the shader will use the currently active material and light0 so set them
-    ngl::Material m( ngl::STDMAT::GOLD );
+    //ngl::Material m( ngl::STDMAT::GOLD );
     // load our material values to the shader into the structure material (see Vertex shader)
-    m.loadToShader( "material" );
+    //m.loadToShader( "material" );
+
+    createCube(0.2);
+    loadTexture();
+    m_text.reset( new ngl::Text(QFont("Arial",14)));
+    m_text->setScreenSize(width(),height());
+
     // Now we will create a basic Camera from the graphics library
     // This is a static camera so it only needs to be set once
     // First create Values for the camera position
-    ngl::Vec3 from( 1, 1, 1 );
+    ngl::Vec3 from( 0, 1, 2 );
     ngl::Vec3 to( 0, 0, 0 );
     ngl::Vec3 up( 0, 1, 0 );
     // now load to our new camera
@@ -82,16 +187,6 @@ void NGLScene::initializeGL()
     // set the shape using FOV 45 Aspect Ratio based on Width and Height
     // The final two are near and far clipping planes of 0.5 and 10
     m_cam.setShape( 45.0f, 720.0f / 576.0f, 0.05f, 350.0f );
-    shader->setUniform( "viewerPos", m_cam.getEye().toVec3() );
-    // now create our light that is done after the camera so we can pass the
-    // transpose of the projection matrix to the light to do correct eye space
-    // transformations
-    ngl::Mat4 iv = m_cam.getViewMatrix();
-    iv.transpose();
-    ngl::Light light( ngl::Vec3( -2, 5, 2 ), ngl::Colour( 1, 1, 1, 1 ), ngl::Colour( 1, 1, 1, 1 ), ngl::LightModes::POINTLIGHT );
-    light.setTransform( iv );
-    // load these values to the shader as well
-    light.loadToShader( "light" );
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -109,19 +204,9 @@ void NGLScene::loadMatricesToShader()
 {
     ngl::ShaderLib* shader = ngl::ShaderLib::instance();
 
-    ngl::Mat4 MV;
-    ngl::Mat4 MVP;
-    ngl::Mat3 normalMatrix;
-    ngl::Mat4 M;
-    M            = m_mouseGlobalTX;
-    MV           = M * m_cam.getViewMatrix();
-    MVP          = M * m_cam.getVPMatrix();
-    normalMatrix = MV;
-    normalMatrix.inverse();
-    shader->setUniform( "MV", MV );
-    shader->setUniform( "MVP", MVP );
-    shader->setUniform( "normalMatrix", normalMatrix );
-    shader->setUniform( "M", M );
+    ngl::Mat4 MVP=m_transform.getMatrix() *m_mouseGlobalTX*m_cam.getVPMatrix();
+
+    shader->setRegisteredUniform("MVP",MVP);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -131,7 +216,7 @@ void NGLScene::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glViewport(0,0,m_win.width,m_win.height);
-    ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
+    //ngl::VAOPrimitives *prim=ngl::VAOPrimitives::instance();
 
     ngl::ShaderLib *shader=ngl::ShaderLib::instance();
     (*shader)["Phong"]->use();
@@ -151,11 +236,59 @@ void NGLScene::paintGL()
 
 	loadMatricesToShader();
 
-    prim->draw("cube");
+    // now we bind back our vertex array object and draw
+    glBindVertexArray(m_vaoID);		// select first VAO
+
+    int instances=0;
+    // need to bind the active texture before drawing
+    glBindTexture(GL_TEXTURE_2D,m_textureName);
+    glPolygonMode(GL_FRONT_AND_BACK,m_polyMode);
+
+    //looping through to create a grid of cubes will replace with particle system
+    for (float z=-14; z<15; z+=0.5)
+    {
+        for (float x=-14; x<15; x+=0.5)
+        {
+            m_transform.reset();
+            {
+                m_transform.setRotation(x*20,(x*z)*40,z*2);
+                m_transform.setPosition(x,0.49,z);
+                loadMatricesToShader();
+                ++instances;
+                glDrawArrays(GL_TRIANGLES, 0,36 );	// draw object
+            }
+        }
+    }
+    // calculate and draw FPS
+    ++m_frames;
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    m_text->setColour(0,0,0);
+    QString text=QString("Particles %1  %2 fps").arg(instances).arg(m_fps);
+    m_text->renderText(10,20,text);
+
+    //before textures
+    //prim->draw("cube");
 }
 
 NGLScene::~NGLScene()
 {
+    // remove the texture now we are done
+    glDeleteTextures(1,&m_textureName);
+}
+
+void NGLScene::timerEvent(QTimerEvent *_event)
+{
+    if(_event->timerId() == m_fpsTimer)
+        {
+            if( m_timer.elapsed() > 1000.0)
+            {
+                m_fps=m_frames;
+                m_frames=0;
+                m_timer.restart();
+            }
+         }
+    // re-draw GL
+    update();
 }
 
 void NGLScene::toggleSnow(bool _snow)
